b228bd557cb2165b446d23d167543b6e86cf3338
mohit_mourya <hellomohit98@gmail.com>
2024-07-27 23:10:41 +0530

    fix cors error

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+// Use CORS middleware
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World')
